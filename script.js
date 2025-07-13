21bd0595f898e1079ecc1f0a8f902d2241debe26
Ammar Multani <ammarmultani3130@gmail.com>
2025-07-13 20:20:26 +0530

    updated files

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const scroll = new LocomotiveScroll({
 	el: document.querySelector('[data-scroll-container]'),
 	smooth: true,
-	multiplier: 1.2,
-	smartphone: {
-		smooth: true,
-	},
-	tablet: {
-		smooth: true,
-	},
 });
 
 function toggleMenu() {
